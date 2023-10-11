fa95552271003c8ad009567a53167e16bf5cfb53
Bjoern Doebel <doebel@amazon.de>
2023-10-11 15:59:05 +0000

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,13 @@
 return {
+    'neanias/everforest-nvim',
+    'AlexvZyl/nordic.nvim',
+    'maxmx03/solarized.nvim',
+    'mrjones2014/lighthaus.nvim',
+    'navarasu/onedark.nvim',
+    'ellisonleao/gruvbox.nvim',
+    'nyoom-engineering/oxocarbon.nvim',
+    'EdenEast/nightfox.nvim',
+    'folke/tokyonight.nvim',
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
